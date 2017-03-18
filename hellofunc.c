9fb87bafcd386dd4949f7573b05f95f691eca1eb
SPRATHIN <sprathin007@gmail.com>
2017-03-18 14:53:36 +0530

    Add files via upload

---
+++ hellofunc.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<hellomake.h>
+void MyPrintHelloMake(void)
+{
+printf("hello\n");
+return;
+}
---
+++ hellomake.c
@@ -0,0 +1,6 @@
+#include <hellomake.h>
+int main() {
+// call a function in another file
+MyPrintHelloMake();
+return(0);
+}
